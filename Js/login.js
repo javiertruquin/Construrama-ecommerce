a7853fbfd7fbeaedc5284ea8aba38fd6d036cb48
lrlichardi <lrlichardi@hotmail.com>
2021-04-03 18:37:11 -0300

    modificacion de navbar en index y log in ..se agrega modal registro

--- Js/login.js
+++ Js/login.js
@@ -3,31 +3,71 @@ const usuario = document.getElementById('usuarioUI');
 const contra = document.getElementById('contraUI');
 const navbar = document.getElementById('navbarUI');
 const logInNav = document.getElementById('logInNavUI');
-const json = localStorage.getItem('usuarios'); 
-let usuarios = JSON.parse(json) || []; 
+const registrarse = document.getElementById('registrarseUI');
+// REGISTRO USUARIO 
+const formularioForm = document.getElementById('registro');
+const usuarioInput = document.getElementById('inputUsuario');
+const nombreInput = document.getElementById('inputNombre');
+const correoInput = document.getElementById('inputCorreo');
+const passInput = document.getElementById('inputPass');
+// CONVERSION DE STRING A ARRAY Y AL REVES
+const json = localStorage.getItem('usuarios');
+let usuarios = JSON.parse(json) || [];
+let usuarioId = "";
 
+function generarID() {
 
-function validarUsuarioSubmit(e){
+    return '_' + Math.random().toString(36).substr(2, 9);
+};
+
+function validarUsuarioSubmit(e) {
     e.preventDefault();
     const usuarioValido = usuarios.find((lUsuario) => lUsuario.usuario === usuario.value && lUsuario.pass === contra.value);
     console.log(usuarioValido);
-        if (usuarioValido){
-        alert("Bienvenido " + usuarioValido.nombre );
-        let userLog = []
-         const logUsuario = `<a class="nav-link active text-white" aria-current="page" href="#">${usuarioValido.usuario}</a>
-            `;
-        userLog.push(logUsuario);
+    if (usuarioValido) {
+        alert("Bienvenido " + usuarioValido.nombre);
+        let userLog = [];
+        if (usuarioValido.rol === 'Administrador') {
+            let logUsuario = `
+         <a class="nav-link active text-white" aria-current="page" href="./admin-productos.html">Modificar Productos</a>
+         <li><a class="nav-link active text-white" aria-current="page" href="#">USUARIO: ${usuarioValido.usuario}</a></li
+                    `;
+            userLog.push(logUsuario);
+        }
+        else {
+            let logUsuario = `
+         <a class="nav-link active text-white" aria-current="page" href="#">USUARIO: ${usuarioValido.usuario}</a>
+                    `;
+            userLog.push(logUsuario);
+        }
         console.log(userLog);
         logInNav.style.display = "none";
+        registrarse.style.display = "none";
         navbar.innerHTML = userLog.join('');
-        window.location="index.html";
-
-            }
+        // window.location="index.html";
+    }
     else {
-          alert("Usuario o contraseña no coinciden");
-         }
-         
-   }
+        alert("Usuario o contraseña no coinciden");
+    }
+}
 
+formularioForm.onsubmit = function (e) {
+    e.preventDefault();
+    const usuario = {
+        id: generarID(),
+        usuario: usuarioInput.value,
+        nombre: nombreInput.value,
+        correo: correoInput.value,
+        pass: passInput.value,
+        rol: "usuario",
+    };
+    usuarios.push(usuario);
+    const json = JSON.stringify(usuarios); // Convertir datos a un string JSON.
+    localStorage.setItem('usuarios', json);
+    const myModal = document.getElementById('formulario')
+    const modal = bootstrap.Modal.getInstance(myModal);
+     modal.hide();
+    formularioForm.reset(); // reset limpia los campos del formulario.
+};
 
 login.onsubmit = validarUsuarioSubmit;
